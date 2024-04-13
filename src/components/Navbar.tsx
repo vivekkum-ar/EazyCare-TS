f184384f16aca05a3551c146fcfee97753aba568
vivekkum-ar <vivekkr9119@gmail.com>
2024-04-13 12:17:33 +0530

    Feat: completed navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,62 @@
-import { assets } from "../assets/assets"
-import { NavLink } from 'react-router-dom'
+import { useState } from "react";
+import { assets } from "../assets/assets";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
+
   return (
-    <div className=" flex justify-between items-center py-4 text-sm mb-5 border-b border-b-gray-400">
-      <img className="w-44 cursor-pointer" src={assets.logo} alt="Logo" srcSet="" />
+    <div className="glass px-4 mt-2 flex justify-between items-center py-4 text-sm mb-5 border-b border-b-gray-400">
+      <img
+        className="w-44 cursor-pointer"
+        src={assets.logo}
+        alt="Logo"
+        srcSet=""
+      />
       <ul className="hidden md:flex items-start gap-5 font-medium">
-        <NavLink to={'/home'}>
+        <NavLink to={"/home"}>
           <li className="py-1 uppercase">Home</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to={'/doctors'}>
+        <NavLink to={"/doctors"}>
           <li className="py-1 uppercase">All Doctors</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to={'/about'}>
+        <NavLink to={"/about"}>
           <li className="py-1 uppercase">About</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to={'/contact'}>
+        <NavLink to={"/contact"}>
           <li className="py-1 uppercase">Contact</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
       <div className="flex items-center gap-4">
-      <button className="bg-primary text-white font-light hidden px-8 py-3 rounded-full md:block ">
-        Create Account
-      </button>
+        {token ? (
+          <div className="flex items-center gap-2 cursor-pointer group relative">
+            <img className="w-8 rounded-full" src={assets.profile_pic} alt=""/>
+            <img className="p-2.5" src={assets.dropdown_icon} alt=""/>
+            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+              <div className="min-w-48 bg-stone-100 rounded-xl flex flex-col gap-4 p-4">
+                <p onClick={() => navigate("my-profile")} className="hover hover:text-black cursor-pointer">My Profile</p>
+                <p onClick={() => navigate("my-appointments")} className="hover hover:text-black cursor-pointer">My Appointments</p>
+                <p onClick={() => setToken(false)} className="hover hover:text-black cursor-pointer">Logout</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="bg-primary text-white font-light hidden px-8 py-3 rounded-full md:block "
+          >
+            Create Account
+          </button>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
