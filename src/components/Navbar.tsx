ee7d8e8a3072ff9c8e249e2317792b48d2c709c4
vivekkum-ar <vivekkr9119@gmail.com>
2024-04-13 10:32:17 +0530

    New: added underline of navbar elements onClick

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,33 @@
-import React from 'react'
 import { assets } from "../assets/assets"
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="Logo" srcSet="" />
-      <ul>
+    <div className=" flex justify-between items-center py-4 text-sm mb-5 border-b border-b-gray-400">
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="Logo" srcSet="" />
+      <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink to={'/home'}>
-          <li className="uppercase">Home</li>
-          <br />
+          <li className="py-1 uppercase">Home</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
-        <NavLink to={'/'}>
-          <li className="uppercase">All Doctors</li>
-          <br />
+        <NavLink to={'/doctors'}>
+          <li className="py-1 uppercase">All Doctors</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
-        <NavLink to={''}>
-          <li className="uppercase">About</li>
-          <br />
+        <NavLink to={'/about'}>
+          <li className="py-1 uppercase">About</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
-        <NavLink to={''}>
-          <li className="uppercase">Contact</li>
-          <br />
-        </NavLink>
-        <NavLink to={''}>
-          <li className="uppercase">Create Account</li>
-          <br />
+        <NavLink to={'/contact'}>
+          <li className="py-1 uppercase">Contact</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
       </ul>
+      <div className="flex items-center gap-4">
+      <button className="bg-primary text-white font-light hidden px-8 py-3 rounded-full md:block ">
+        Create Account
+      </button>
+      </div>
     </div>
   )
 }
