cadae35a9cceed3a115b9cfbd6418a08b24c0098
vivekkum-ar <vivekkr9119@gmail.com>
2024-04-28 09:42:28 +0530

    add context providers and login component to the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import AdminContextProvider from "./context/AdminContext.jsx";
+import DoctorContextProvider from "./context/doctorContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <App />
-    </Routes>
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
   </BrowserRouter>
 );
