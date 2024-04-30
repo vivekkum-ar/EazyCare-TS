19a1ad9b6b0ca40a69c69651a0ffaec848d166c2
vivekkum-ar <vivekkr9119@gmail.com>
2024-04-30 21:34:48 +0530

    Feat: Enhance Navbar to include user data in profile display

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import { toast } from "react-toastify";
 const Navbar = () => {
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
-  const { token,setToken } = useContext(AppContext)
+  const { token,setToken, userData } = useContext(AppContext)
 
   const logout = async () => {
       setToken(false)
@@ -50,9 +50,9 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-4">
-        {token ? (
+        {token && userData ? (
           <div className="flex items-center gap-2 cursor-pointer group relative">
-            <img className="hidden md:block w-8 rounded-full" src={assets.profile_pic} alt=""/>
+            <img className="hidden md:block w-8 rounded-full" src={userData.image} alt={userData.name}/>
             <img className="p-2.5" src={assets.dropdown_icon} alt=""/>
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded-xl flex flex-col gap-4 p-4">
