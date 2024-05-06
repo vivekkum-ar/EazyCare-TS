42120ef80659af2341a708c380d0286785865957
vivekkum-ar <vivekkr9119@gmail.com>
2024-05-06 12:10:32 +0530

    Feat: add appointment cancellation API and dashboard data retrieval for admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,6 +4,7 @@ import {v2 as cloudinary} from "cloudinary"
 import doctorModel from "../model/doctorModel.js"
 import jwt from "jsonwebtoken"
 import appointmentModel from "../model/appointmentModel.js"
+import userModel from "../model/userModel.js"
 /* ------------------------------------ API for adding doctor ----------------------------------- */
 
 const addDoctor = async (req,res) => {
@@ -126,6 +127,58 @@ const appointmentsAdmin = async (req,res) => {
     }
 }
 
+/* ------------------------------ API for appointment cancellation ------------------------------ */
+const appointmentCancel = async (req,res) => {
+    try {
+        const { appointmentId } = req.body;
+        const appointmentData = await appointmentModel.findById(appointmentId)
+        
+            await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true})
+            /* ------------------------------------ releasing doctor slot ----------------------------------- */
+            const {docId,slotDate,slotTime} = appointmentData
+            const doctorData = await doctorModel.findById(docId)
+            let slots_booked = doctorData.slots_booked
+            slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime )
+            await doctorModel.findByIdAndUpdate(docId,{slots_booked})
+            res.json({
+                status:true,
+                message: "Appointment Cancelled"
+            })
+    } catch (error) {
+        console.log(error)
+        return res.json({
+            status: false,
+            message: error.message
+        }) 
+    }
+}
+
+/* ---------------------------------- API to get Dashboard data --------------------------------- */
+const adminDashboard = async (req,res) => {
+    try {
+        const doctors = await doctorModel.find({})
+        const users = await userModel.find({})
+        const appointments = await appointmentModel.find({})
+
+        const dashData = {
+            doctors: doctors.length,
+            appointments: appointments.length,
+            patients: users.length,
+            latestAppointments: appointments.reverse().slice(0,5)
+        }
+
+        return res.json({
+            success: true,
+            dashData
+        })
+    } catch (error) {
+        return res.json({
+            status: false,
+            message: error.message
+        }) 
+    }
+}
 
 
-export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin }; 
+
+export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin, appointmentCancel ,adminDashboard}; 
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addDoctor, allDoctors, appointmentsAdmin, loginAdmin } from "../controllers/adminController.js"
+import { addDoctor, adminDashboard, allDoctors, appointmentCancel, appointmentsAdmin, loginAdmin } from "../controllers/adminController.js"
 import upload from "../middlewares/multer.js"
 import authAdmin from "../middlewares/authAdmin.js"
 import { changeAvilability } from "../controllers/doctorController.js"
@@ -11,5 +11,7 @@ adminRouter.post("/login",loginAdmin)
 adminRouter.post("/all-doctors",authAdmin,allDoctors)
 adminRouter.post("/change-availability",authAdmin,changeAvilability)
 adminRouter.get("/appointments",authAdmin,appointmentsAdmin)
+adminRouter.post("/cancel-appointment",authAdmin,appointmentCancel)
+adminRouter.get("/dashboard",authAdmin,adminDashboard)
 
 export default adminRouter
